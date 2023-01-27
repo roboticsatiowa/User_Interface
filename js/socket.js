1d175b36774a1453bf4e399978cc505f6627330c
Pastachilli <gpostacchini@gmail.com>
2023-01-26 19:41:47 -0600

    Added 3 sockets rather than 1 for the video feed. New sockets are "video1", "video2", "video3" instead of "video"

--- js/socket.js
+++ js/socket.js
@@ -11,10 +11,25 @@ io.on('connection', function (socket) {
   socket.on('name', (name) => {
     console.log(`connected ${name}`)
 
-    if (name == "video") {
-      socket.on('data', function (data) {                     // listen on client emit 'data'
+    // Video sockets
+    if (name == "video1") {
+      socket.on('data', function (data) {                     // Camera 1
         const imgElem1 = document.getElementById("camera1")
+        imgElem1.src = `data:image/jpeg;base64,${data}`
+        imgElem2.src = `data:image/jpeg;base64,${data}`
+        imgElem3.src = `data:image/jpeg;base64,${data}`
+      })
+    }
+    if (name == "video2") {
+      socket.on('data', function (data) {                     // Camera 2
         const imgElem2 = document.getElementById("camera2")
+        imgElem1.src = `data:image/jpeg;base64,${data}`
+        imgElem2.src = `data:image/jpeg;base64,${data}`
+        imgElem3.src = `data:image/jpeg;base64,${data}`
+      })
+    }
+    if (name == "video3") {
+      socket.on('data', function (data) {                     // Camera 3
         const imgElem3 = document.getElementById("camera3")
         imgElem1.src = `data:image/jpeg;base64,${data}`
         imgElem2.src = `data:image/jpeg;base64,${data}`
